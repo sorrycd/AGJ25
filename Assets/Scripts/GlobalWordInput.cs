e1a45bf543512839553f71c3aacbb1b0311c24cd
Simply-Aidan <brendanlynnofficial@gmail.com>
2025-09-27 18:34:18 -0700

    actions have consequences (in the debug log)

--- Assets/Scripts/GlobalWordInput.cs
+++ Assets/Scripts/GlobalWordInput.cs
@@ -1,11 +1,40 @@
 using UnityEngine;
 using TMPro;
-
+using System;
+using System.Collections.Generic;
 public class GlobalWordInput : MonoBehaviour
 {
     private string currentWord = "";
+    private Dictionary<string,Action> wordActions;
     public TextMeshPro Input3DText; 
-
+    void Start()
+    {
+        wordActions = new Dictionary<string, Action>()
+        {
+            {"pow", Punch},
+            {"clank", Punch},
+            {"thud", Punch},
+            {"wack", Punch},
+            {"slam", Punch},
+            {"clang", Punch},
+            {"kapow", Punch},
+            {"wham", Punch},
+            {"achoo", Sneeze},
+            {"bang", Shoot},
+            {"boom", Shoot},
+            {"bam", Shoot},
+            {"blam", Shoot},
+            {"ratatat",RapidFire},
+            {"woosh", Dash},
+            {"swoosh", Dash},
+            {"zap", Laser},
+            {"stab", Knife},
+            {"swing",Knife},
+            {"schwing",Knife},
+            {"slash",Knife},
+            {"snip", BoltCutters}   
+        };
+    }
     void Update()
     {
         foreach (char c in Input.inputString)
@@ -20,6 +49,7 @@ public class GlobalWordInput : MonoBehaviour
             }
             else if (c == ' ')
             {
+                SubmitWord();
                 currentWord = ""; // reset after submit
             }
         }
@@ -27,4 +57,26 @@ public class GlobalWordInput : MonoBehaviour
         // Show typed word above the object
         Input3DText.text = currentWord;
     }
+    void SubmitWord()
+    {
+        if(wordActions.ContainsKey(currentWord))
+        {  
+            Debug.Log("Matched Word: "+currentWord);
+            wordActions[currentWord].Invoke();
+        }
+        else
+        {  
+            Debug.Log("No Action for: "+currentWord);
+        }
+        currentWord = "";
+    }
+    void Punch() { Debug.Log("Player Punches!"); }
+    void Shoot() { Debug.Log("Player Shoots!"); }
+    void RapidFire() { Debug.Log("Player RapidFires!"); }
+    void Knife() { Debug.Log("Player Stabs!"); }
+    void Sneeze() { Debug.Log("Player Sneezes!"); }
+
+    void Dash() { Debug.Log("Player Dashes"); }
+    void Laser() { Debug.Log("Player Shoots a Laser"); }
+    void BoltCutters() { Debug.Log("Player tries to use bolt cutters"); }
 }
